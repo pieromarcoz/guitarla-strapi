73d15d4f5640959195818229b521131aeeaef3a5
piero965d <piero965d@gmail.com>
2023-01-11 02:07:39 -0500

    deployment aws

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 33060),
+      host: env('DATABASE_HOST', 'bd-strapi-api.ck6reof24v4s.us-east-1.rds.amazonaws.com'),
+      port: env.int('DATABASE_PORT', 33016),
       database: env('DATABASE_NAME', 'guitarla-strapi'),
       user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'root'),
+      password: env('DATABASE_PASSWORD', 'pieromarcoz'),
       ssl: env.bool('DATABASE_SSL', true),
     },
   },
--- config/plugins.js
+++ config/plugins.js
@@ -1,15 +1,18 @@
 module.exports = ({ env }) => ({
     upload: {
         config: {
-        provider: 'cloudinary',
+        provider: ['cloudinary', 'aws-s3'],
         providerOptions: {
             cloud_name: env('CLOUDINARY_NAME'),
             api_key: env('CLOUDINARY_KEY'),
             api_secret: env('CLOUDINARY_SECRET'),
+            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: env('AWS_ACCESS_SECRET'),
+            region: env('AWS_REGION'),
         },
         actionOptions: {
-            upload: {},
-            uploadStream: {},
+            upload: {ACL: null},
+            uploadStream: {ACL: null},
             delete: {},
         },
         },
